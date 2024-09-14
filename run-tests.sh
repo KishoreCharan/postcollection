77367b5d29f05f2011bcc5397a7d6ecd0941a1c8
Kishore Basam <96798391+KishoreCharan@users.noreply.github.com>
2024-09-14 15:30:07 +1000

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -12,4 +12,4 @@ npm install -g newman
 
 # Run the Postman collection with Newman
 # Replace 'collection.json' and 'environment.json' with your actual file names
-newman run collection.json -e environment.json --reporters cli,html --reporter-html-export newman-report.html
+newman run Kishore.postman_collection.json --reporters cli,html --reporter-html-export newman-report.html
